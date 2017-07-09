cb19822a79b3a4b84a8746fc4bb65ddb77d9dd9f
Kirill Demidov <kademidov@gmail.com>
2017-07-09 22:14:22 +0300

    update

--- src/js/scripts/wubook.js
+++ src/js/scripts/wubook.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 var WuBook= new _WuBook(1492614572);
   var wbparams= {
         lang: 'ru',
-        layout: 'demetra',
+        layout: '',
         width: 1140,
         height: 'auto',
         mobile: 1,
