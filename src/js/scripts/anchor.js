36ffb89cd4d3c32b78a22320e018d8993ce7273d
Kirill Demidov <kademidov@gmail.com>
2017-07-09 21:30:31 +0300

    add booking system

---
+++ src/js/scripts/anchor.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+
+  function anchorScroll(boxAnchorLink){
+    $(boxAnchorLink + ' a').on('click', function(e){
+        e.preventDefault();
+        var attr = $(this).attr('href').substring(1);
+        var currentPosition = $(document).scrollTop();
+        var idPosition = $('#'+attr).offset().top;
+        var scrollTime = Math.abs(currentPosition - idPosition) / 4; // Math.abs - модуль числа.
+        $('body,html').animate({'scrollTop':idPosition-70},scrollTime);
+    });
+  };
+
+  anchorScroll('.page-header__main-nav');
+
+});
---
+++ src/js/scripts/wubook.js
@@ -0,0 +1,10 @@
+var WuBook= new _WuBook(1492614572);
+  var wbparams= {
+        lang: 'ru',
+        layout: 'demetra',
+        width: 1140,
+        height: 'auto',
+        mobile: 1,
+        mheight: 640
+  };
+  WuBook.design_iframe("_wbord_", wbparams);
