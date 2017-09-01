f332de2a4b3513922c9d4b57dc55fb24ea09583a
Kirill Demidov <kademidov@gmail.com>
2017-09-01 13:29:10 +0300

    обновил js

--- src/js/scripts/header-scroll.js
+++ src/js/scripts/header-scroll.js
@@ -1,9 +1,24 @@
 $(window).on('scroll', function(e) {
 
   if ( $(window).scrollTop() > 170 ) {
+
     $('.page-header').addClass('page-header--scroll');
+    $('.rooms-slider').css({
+      "padding-top": "253px"
+    });
+
+    $('.promo').css({
+      "margin-top": "143px"
+    });
   } else {
     $('.page-header').removeClass('page-header--scroll');
+    $('.rooms-slider').css({
+      "padding-top": "175px"
+    });
+
+    $('.promo').css({
+      "margin-top": "0"
+    });
   }
 
 });
